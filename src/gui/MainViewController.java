87da08a1f1bf08b1c4bad2229bd544f24d00ec84
Luiz Felipe <luizfelipealvesneves3@gmail.com>
2026-03-17 20:36:57 -0400

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -35,7 +35,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -62,9 +62,6 @@ public class MainViewController implements Initializable{
 		mainVBox.getChildren().add(mainMenu);
 		mainVBox.getChildren().addAll(newVBox.getChildren());
 
-
-		
-		
 		
 	}catch (IOException e) {
 		Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
